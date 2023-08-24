dbaa29f73a527195b0755a801da6a46e988acd0e
BeltranPena <bpena@alumnos.exa.unicen.edu.ar>
2023-08-23 21:14:49 -0300

    Invitacion cumpleaños

--- js/contador.js
+++ js/contador.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var flipdown = new FlipDown(1696690800,{
+    var flipdown = new FlipDown(1696561200,{
       headings:["Dias", "Horas", "Minutos", "Segundos"]})
   
       // Start the countdown
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
       // Do something when the countdown ends
       flipdown.ifEnded(() => {
         document.querySelector("#flipdown").innerHTML = '';
-        // document.querySelector("#video").innerHTML = 
-        // `<video height="180px" width="320px" controls=1 style="margin-top:-55px; margin-bottom:-20px;">
-        //   <source src="musica/Es hoy.mp4" type="video/mp4">
-        // </video>`;
+        document.querySelector(".arrow").innerHTML = '';
+         document.querySelector("#video").innerHTML = 
+         `<video height="180px" width="320px" controls=1 style="margin-top:-55px; margin-bottom:-20px;">
+           <source src="images/joey1.mp4" type="video/mp4">
+         </video>`;
       })})
